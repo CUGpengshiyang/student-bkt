3d67c6406a3af678ca3d747bc774485c8586c4cd
Karthick Arya <karthickarya09@gmail.com>
2018-11-26 18:40:14 -0500

    Individual KC and threshold analysis

--- bktModel.py
+++ bktModel.py
@@ -1,20 +1,29 @@
 import pandas as pd
 import numpy as np
-
+import matplotlib.pyplot as plt
+x=[]
+y=[]
+# Data is read from file
 data = pd.read_csv('AssignmentData.csv')
+
+# Appending pC column and pKC for each KC and initializing them with value -1
 temp = pd.DataFrame(np.zeros(((len(data)))), columns=['pC'])
 data = data.join(temp)
 temp = np.empty(((len(data)), 7))
 temp[:] = -1
 temp = pd.DataFrame(temp, columns=['pKC_1', 'pKC_27', 'pKC_24', 'pKC_14', 'pKC_22', 'pKC_20', 'pKC_21'])
 data = data.join(temp)
+
+#Values of the parameters
 pInit = 0.5
 pTransit = 0.1 
 pSlip = 0.1
 pGuess = 0.1
 threshold = 0.6
-
+#List of unique students based on Student column
 students = data.Student.unique()
+
+#For each student, obtain steps for each KC seperately and loop through each KC to calculate pC and pKC for each step for that KC
 for student in students:
     pLNext = pInit
     studentData = data[data.Student==student]
@@ -36,7 +45,28 @@ for student in students:
             
             pLNext = pLNext + ((1-pLNext)*pTransit)
             pC = (pLNext*(1-pSlip))+((1-pLNext)*pGuess)
+            
+            #Updating the respective pKC and pC values in main data 
             data.iat[index, 10] = pC
             data.iat[index, 11+i] = (0,1)[pC>threshold]
 
-data.to_csv('output.csv', index=False)
+#creating a new CSV file with necessary data
+# data.to_csv('output.csv', index=False)
+cols = data.columns.values.tolist()
+cols = data.columns.values.tolist()
+accuracy = []
+correctTotal=0
+totalTotal = 0
+for i in range(7):
+    correct = len(data[(data[cols[2]]==1) & (data[cols[3+i]]==1) & (data[cols[11+i]]==1)]) + len(data[(data[cols[2]]==0) & (data[cols[3+i]]==1) & (data[cols[11+i]]==0)])
+    correctTotal += correct
+    total =  len(data[(data[cols[3+i]]==1) & ((data[cols[2]]==1) | (data[cols[2]]==0))])
+    totalTotal += total 
+    accuracy.append(correct/total)
+
+plt.plot(cols[3:10], accuracy, label=str(correctTotal/totalTotal))
+plt.xlabel('Knowledge Components')
+plt.ylabel('Accuracy')
+plt.ylim(ymax=0.93, ymin=0.84)
+plt.legend()
+plt.show()
